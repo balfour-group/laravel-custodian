0fa7c8ae9461dc7d00ca5426f750396d56a9f611
Matthew Goslett <matthew@goslett.co.za>
2019-04-27 14:13:04 +0200

    require laravel 5.8+

--- src/migrations/2019_03_06_142500_create_roles_table.php
+++ src/migrations/2019_03_06_142500_create_roles_table.php
@@ -12,7 +12,7 @@ class CreateRolesTable extends Migration
     public function up()
     {
         Schema::create('roles', function (Blueprint $table) {
-            $table->increments('id');
+            $table->bigIncrements('id');
             $table->string('name')->unique();
             $table->json('permissions')->nullable();
             $table->timestamps();
--- src/migrations/2019_03_06_143400_create_role_user_table.php
+++ src/migrations/2019_03_06_143400_create_role_user_table.php
@@ -12,9 +12,9 @@ class CreateRoleUserTable extends Migration
     public function up()
     {
         Schema::create('role_user', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('role_id')->unsigned();
-            $table->integer('user_id')->unsigned();
+            $table->bigIncrements('id');
+            $table->bigInteger('role_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
             $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
             $table->foreign('user_id')->references('id')->on('users');
         });
